37aebb72efc1e7e7ff15a8ea78d586a8f7b9e310
paul-zelik <betinellipaul@gmail.com>
2025-06-09 18:18:24 +0200

    Ajout des opérandes

--- src/main.c
+++ src/main.c
@@ -3,49 +3,129 @@
 #include <string.h>
 #include <stdio.h>
 
-const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "MD Viewer";
+const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Markdown";
 const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-// === Données Markdown ===
-// (Remplace ici par ton propre contenu Markdown)
-
-const char *fichiers[] = {
-  "Fichier1.md", "Guide.md", "Notes.md", "Intro.md", "Cours1.md", "Projet.md"
-};
-
-const char *contenus[] = {
-  "## Fichier 1\nCeci est le contenu du fichier 1.\nAvec plusieurs lignes.\n...\n",
-  "# Guide\nVoici un guide d’utilisation.\nLigne 2.\nLigne 3.\n",
-  "# Notes diverses\n- Point 1\n- Point 2\n",
-  "# Intro\nBienvenue dans ce Markdown.",
-  "# Cours 1\nChapitre 1...\nChapitre 2...",
-  "# Projet\nInstructions...\nÉtapes...\nFin."
-};
-
-#define NB_FICHIERS (sizeof(fichiers)/sizeof(fichiers[0]))
-#define NB_PAR_LIGNE 3
-#define LIGNES_MAX 100
+#define MAX_LIGNES 100
 #define LIGNES_VISIBLE 20
 
-char *lignes[LIGNES_MAX];
+char *lignes[MAX_LIGNES];
 size_t total_lignes = 0;
+
 
 void parse_markdown(const char *data) {
   total_lignes = 0;
   const char *debut = data;
 
-  while (*debut && total_lignes < LIGNES_MAX) {
+  while (*debut && total_lignes < MAX_LIGNES) {
     const char *fin = strchr(debut, '\n');
     if (!fin) fin = debut + strlen(debut);
 
     size_t taille = fin - debut;
-    lignes[total_lignes] = malloc(taille + 1);
-    if (lignes[total_lignes]) {
-      strncpy(lignes[total_lignes], debut, taille);
-      lignes[total_lignes][taille] = '\0';
-      total_lignes++;
+    char *ligne_brute = (char *)malloc(taille + 1);
+    if (!ligne_brute) break;
+
+    strncpy(ligne_brute, debut, taille);
+    ligne_brute[taille] = '\0';
+
+    // Ligne transformée
+    char *ligne_parsee = (char *)malloc(256);
+    if (!ligne_parsee) {
+      free(ligne_brute);
+      break;
+    }
+    ligne_parsee[0] = '\0';
+
+    // # Titre ou ## Sous-titre
+    if (strncmp(ligne_brute, "# ", 2) == 0) {
+      strcat(ligne_parsee, "## ");
+      for (size_t i = 2; i < strlen(ligne_brute); i++)
+        ligne_parsee[strlen(ligne_parsee)] = toupper(ligne_brute[i]);
+      ligne_parsee[strlen(ligne_parsee)] = '\0';
+    } else if (strncmp(ligne_brute, "## ", 3) == 0) {
+      strcat(ligne_parsee, "# ");
+      for (size_t i = 3; i < strlen(ligne_brute); i++)
+        ligne_parsee[strlen(ligne_parsee)] = toupper(ligne_brute[i]);
+      ligne_parsee[strlen(ligne_parsee)] = '\0';
     }
 
+    // > Citation
+    else if (strncmp(ligne_brute, "> ", 2) == 0) {
+      snprintf(ligne_parsee, 255, "│ %s", ligne_brute + 2);
+    }
+
+    // - Liste
+    else if (strncmp(ligne_brute, "- ", 2) == 0) {
+      snprintf(ligne_parsee, 255, "➤ %s", ligne_brute + 2);
+    }
+
+    // Ligne classique + mise en forme dans le texte
+    else {
+      char *src = ligne_brute;
+      while (*src && strlen(ligne_parsee) < 240) {
+        // Gras : **texte**
+        if (strncmp(src, "**", 2) == 0) {
+          src += 2;
+          while (*src && strncmp(src, "**", 2) != 0) {
+            ligne_parsee[strlen(ligne_parsee)] = toupper(*src);
+            src++;
+          }
+          src += 2;
+        }
+
+        // Italique : *texte*
+        else if (*src == '*') {
+          src++;
+          strcat(ligne_parsee, "/");
+          while (*src && *src != '*') {
+            strncat(ligne_parsee, (char[]){*src, '\0'}, 1);
+            src++;
+          }
+          strcat(ligne_parsee, "/");
+          if (*src == '*') src++;
+        }
+
+        // Code inline : `texte`
+        else if (*src == '`') {
+          src++;
+          strcat(ligne_parsee, "[");
+          while (*src && *src != '`') {
+            strncat(ligne_parsee, (char[]){*src, '\0'}, 1);
+            src++;
+          }
+          strcat(ligne_parsee, "]");
+          if (*src == '`') src++;
+        }
+
+        // Lien [texte](url)
+        else if (*src == '[') {
+          src++;
+          char texte[64] = "", url[64] = "";
+          size_t i = 0;
+          while (*src && *src != ']' && i < 63) texte[i++] = *src++;
+          texte[i] = '\0';
+          if (*src == ']') src++;
+          if (*src == '(') {
+            src++;
+            i = 0;
+            while (*src && *src != ')' && i < 63) url[i++] = *src++;
+            url[i] = '\0';
+            if (*src == ')') src++;
+          }
+          snprintf(ligne_parsee + strlen(ligne_parsee), 255 - strlen(ligne_parsee), "%s (%s)", texte, url);
+        }
+
+        // Normal
+        else {
+          ligne_parsee[strlen(ligne_parsee)] = *src;
+          src++;
+        }
+      }
+      ligne_parsee[strlen(ligne_parsee)] = '\0';
+    }
+
+    lignes[total_lignes++] = ligne_parsee;
+    free(ligne_brute);
     debut = (*fin) ? fin + 1 : fin;
   }
 }
@@ -57,15 +137,15 @@ void liberer_lignes() {
   total_lignes = 0;
 }
 
-void afficher_lignes(size_t offset) {
+void afficher_lignes(size_t debut) {
   eadk_display_push_rect_uniform(eadk_screen_rect, eadk_color_white);
 
   for (size_t i = 0; i < LIGNES_VISIBLE; i++) {
-    size_t idx = offset + i;
-    if (idx >= total_lignes) break;
+    size_t ligne_index = debut + i;
+    if (ligne_index >= total_lignes) break;
 
     eadk_display_draw_string(
-      lignes[idx],
+      lignes[ligne_index],
       (eadk_point_t){2, (uint16_t)(i * 12)},
       false,
       eadk_color_black,
@@ -74,15 +154,16 @@ void afficher_lignes(size_t offset) {
   }
 }
 
-// === Affichage du contenu d’un fichier ===
-void afficher_fichier(size_t index) {
-  parse_markdown(contenus[index]);
+void afficher_fichier() {
+  parse_markdown(eadk_external_data);
 
   size_t offset = 0;
+
   while (1) {
     afficher_lignes(offset);
-    eadk_keyboard_state_t keys = eadk_keyboard_scan();
     eadk_timing_msleep(100);
+
+    eadk_keyboard_state_t keys = eadk_keyboard_scan();
 
     if (eadk_keyboard_key_down(keys, eadk_key_down)) {
       if (offset + LIGNES_VISIBLE < total_lignes) offset++;
@@ -98,48 +179,10 @@ void afficher_fichier(size_t index) {
   liberer_lignes();
 }
 
-// === Affichage du menu wrap (gauche à droite) ===
-void afficher_menu(size_t selection) {
-  eadk_display_push_rect_uniform(eadk_screen_rect, eadk_color_white);
 
-  for (size_t i = 0; i < NB_FICHIERS; i++) {
-    int ligne = i / NB_PAR_LIGNE;
-    int col = i % NB_PAR_LIGNE;
 
-    eadk_point_t pos = {10 + col * 100, (uint16_t)(10 + ligne * 20)};
-    eadk_color_t texte = (i == selection) ? eadk_color_white : eadk_color_black;
-    eadk_color_t fond = (i == selection) ? eadk_color_blue : eadk_color_white;
 
-    eadk_display_draw_string(fichiers[i], pos, false, texte, fond);
-  }
-}
-
-// === Main principal ===
 int main(int argc, char *argv[]) {
-  size_t selection = 0;
-
-  while (1) {
-    afficher_menu(selection);
-    eadk_keyboard_state_t keys = eadk_keyboard_scan();
-    eadk_timing_msleep(100);
-
-    if (eadk_keyboard_key_down(keys, eadk_key_left)) {
-      if (selection > 0) selection--;
-    }
-    if (eadk_keyboard_key_down(keys, eadk_key_right)) {
-      if (selection < NB_FICHIERS - 1) selection++;
-    }
-    if (eadk_keyboard_key_down(keys, eadk_key_down)) {
-      if (selection + NB_PAR_LIGNE < NB_FICHIERS) selection += NB_PAR_LIGNE;
-    }
-    if (eadk_keyboard_key_down(keys, eadk_key_up)) {
-      if (selection >= NB_PAR_LIGNE) selection -= NB_PAR_LIGNE;
-    }
-
-    if (eadk_keyboard_key_down(keys, eadk_key_ok)) {
-      afficher_fichier(selection);
-    }
-  }
-
+  afficher_fichier();
   return 0;
 }

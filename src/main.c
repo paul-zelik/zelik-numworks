16c0ef032335a9b1851e33a9cfab564be9501754
paul-zelik <betinellipaul@gmail.com>
2025-06-14 10:17:53 +0200

    Modif icon + nom

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
-const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Maze";
+const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Zelik";
 const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 #define WIDTH 21
